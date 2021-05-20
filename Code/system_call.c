372ac50c850e0c107886dd41306edcc52613881f
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-05-20 08:12:30 -0400

    Create system_call.c

---
+++ Code/system_call.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <unistd.h>
+ 
+int main(void) {
+  char *binaryPath = "/usr/bin/ping";
+  char *arg1 = "-c1";
+  char *arg2 = "127.0.0.1";
+ 
+  execl(binaryPath, binaryPath, arg1, arg2, NULL);
+  return 0;
+}
