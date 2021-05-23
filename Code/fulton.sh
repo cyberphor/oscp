70b19f413f101e6b7e9cc3832a37dec887389715
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-05-22 22:31:41 -0600

    Update fulton.sh

--- Code/fulton.sh
+++ Code/fulton.sh
@@ -22,3 +22,29 @@ echo "[+] Installed: dirsearch"
 wget https://addons.mozilla.org/firefox/downloads/file/3616824/foxyproxy_standard-7.5.1-an+fx.xpi
 firefox ./foxyproxy_standard-7.5.1-an+fx.xpi
 echo "[+] Installed: foxyproxy"
+
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl*
+curl https://pyenv.run | bash
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+exec "$SHELL"
+pyenv install 3.9.5
+pyenv install 2.7.18
+pyenv global 2.7.18
+echo "[+] Installed: pyenv"
+echo "[+] Configured: python - to be version 2.7.18"
+
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
+python get-pip.py
+python -m pip --version
+echo "[+] Installed: pip"
+python -m pip install --upgrade setuptools
+python -m pip install pysmb
+echo "[+] Installed: pysmb"
+python -m pip install pycryptodome
+echo "[+] Installed: pycryptodome"
+
+sudo apt-get install gcc-multilib
+echo "[+] Installed: gcc-multilib"
