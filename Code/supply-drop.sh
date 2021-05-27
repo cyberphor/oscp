0b11e93152970bbeba197377d6864911bc096c4c
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-05-26 21:38:55 -0600

    Update and rename fulton.sh to supply-drop.sh

--- Code/supply-drop.sh
+++ Code/supply-drop.sh
@@ -4,19 +4,10 @@ echo "[+] Installed: tree"
 sudo apt install burpsuite -y
 echo "[+] Installed: Burp Suite"
 
-sudo apt install mdless -y
+sudo gem install mdless
 echo "[+] Installed: mdless"
 
-#git clone https://github.com/maurosoria/dirsearch
-#sudo cp -R ./dirsearch/ /usr/local/bin/dirsearch/
-
-#SHELL=$(env | grep 'SHELL=' | cut -d'=' -f2)
-#if [ $SHELL == '/bin/bash' ]; then
-#    echo 'export PATH=$PATH:/usr/local/bin/dirsearch' >> ~/.bashrc
-#elif [ $SHELL == '' ]; then
-#    echo 'export PATH=$PATH:/usr/local/bin/dirsearch' >> ~/.bashrc
-#    echo 'zsh'
-#fi
+sudo apt install dirsearch -y
 echo "[+] Installed: dirsearch"
 
 wget https://addons.mozilla.org/firefox/downloads/file/3616824/foxyproxy_standard-7.5.1-an+fx.xpi
