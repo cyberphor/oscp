76b7a5397e99f1d74541fef1e01a95c83fa2c8c2
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-05-29 15:03:39 -0600

    Update configure_flameshot.py

--- Code/configure_flameshot.py
+++ Code/configure_flameshot.py
@@ -13,19 +13,20 @@ def update_flameshot_config(config_file_path,save_path,save_dir):
             else:
                 settings.append(setting)
         new_config = ''.join(settings)
-    print(new_config)
+    with open(config_file_path,"w") as config_file:
+        config_file.write(new_config)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dir")
+    parser.add_argument("--directory")
     parser.add_argument("--reset",action="store_true")
     args = parser.parse_args()
     user_home = os.path.expanduser("~")
     user = os.path.split(user_home)[-1]
     config_file_path = user_home + "/.config/flameshot/flameshot.ini"
     save_path = "savePath="
-    if args.d:
-        save_dir = args.d
+    if args.directory:
+        save_dir = os.path.realpath(args.directory)
     elif args.reset:
         save_dir = user_home + "/Pictures"
     else:
