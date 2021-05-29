650ab80690cb1f2627d06bd5f3717c9a996dd233
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2021-05-29 14:54:48 -0600

    Update configure_flameshot.py

--- Code/configure_flameshot.py
+++ Code/configure_flameshot.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python
 
 import argparse
+import os
 
-def update_flameshot_config(directory):
-    config_file_path = "/home/victor/.config/flameshot/flameshot.ini"
-    savePath = "savePath="
-    savePath_default = savePath + "/home/victor/Pictures\n"
-    savePath_new = savePath + directory + "\n"
+def update_flameshot_config(config_file_path,save_path,save_dir):
     with open(config_file_path,"r") as config_file:
         config = config_file.readlines()
         settings = []
         for setting in config:
-            if savePath in setting:
-                settings.append(savePath_new)
+            if save_path in setting:
+                settings.append(save_path + save_dir + "\n")
             else:
                 settings.append(setting)
         new_config = ''.join(settings)
@@ -20,10 +17,20 @@ def update_flameshot_config(directory):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d")
+    parser.add_argument("--dir")
+    parser.add_argument("--reset",action="store_true")
     args = parser.parse_args()
+    user_home = os.path.expanduser("~")
+    user = os.path.split(user_home)[-1]
+    config_file_path = user_home + "/.config/flameshot/flameshot.ini"
+    save_path = "savePath="
     if args.d:
-        update_flameshot_config(args.d)
+        save_dir = args.d
+    elif args.reset:
+        save_dir = user_home + "/Pictures"
+    else:
+        exit()
+    update_flameshot_config(config_file_path,save_path,save_dir)
 
 if __name__ == "__main__":
     main()
